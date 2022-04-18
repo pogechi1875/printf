9c92e90b843ff6f70ce157167e3d679b6d15ac03
pogechi1875 <ogp1875@gmail.com>
2022-04-17 22:32:14 -0700

    trial of the printf funtion

---
+++ README.h
@@ -0,0 +1,2 @@
+This is my first try at creating a library funtion or even a whole code by myself
+Print_f funtion
---
+++ main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+/**
+ * struct print - structure for printing various types
+ * @t: type to print
+ * @f: function to print
+ */
+typedef struct print
+{
+	char *t;
+	int (*f)(va_list);
+} print_t;
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int print_c(va_list c);
+int print_s(va_list s);
+int print_i(va_list i);
+int print_d(va_list d);
+
+#endif
